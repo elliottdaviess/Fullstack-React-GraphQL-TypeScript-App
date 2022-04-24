0df8a55823945e5614262e82c19e0650524bdb50
elliottdaviess <elliott_davies@live.co.uk>
2022-04-24 20:54:11 +0100

    Logout

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { __prod__, COOKIE_NAME } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -21,17 +21,15 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
-
   app.use(
     cors({
       origin: "http://localhost:3000",
       credentials: true,
     })
   );
-
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -12,6 +12,7 @@ import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 import { EntityManager } from "@mikro-orm/postgresql";
+import { COOKIE_NAME } from "../constants";
 
 @InputType()
 class UsernamePasswordInput {
@@ -42,11 +43,12 @@ class UserResponse {
 export class UserResolver {
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: MyContext) {
-    if (!req.session!.userId) {
+    // you are not logged in
+    if (!req.session.userId) {
       return null;
     }
 
-    const user = await em.findOne(User, { id: req.session!.userId });
+    const user = await em.findOne(User, { id: req.session.userId });
     return user;
   }
 
@@ -92,6 +94,7 @@ export class UserResolver {
         .returning("*");
       user = result[0];
     } catch (err) {
+      //|| err.detail.includes("already exists")) {
       // duplicate username error
       if (err.code === "23505") {
         return {
@@ -105,8 +108,11 @@ export class UserResolver {
       }
     }
 
-    // store user id session - this will set a cookie on the user & keep them logged in
-    req.session!.userId = user.id;
+    // store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
@@ -137,9 +143,27 @@ export class UserResolver {
         ],
       };
     }
-    req.session!.userId = user.id;
+
+    req.session.userId = user.id;
+
     return {
       user,
     };
+  }
+
+  @Mutation(() => Boolean)
+  logout(@Ctx() { req, res }: MyContext) {
+    return new Promise((resolve) =>
+      req.session.destroy((err) => {
+        res.clearCookie(COOKIE_NAME);
+        if (err) {
+          console.log(err);
+          resolve(false);
+          return;
+        }
+
+        resolve(true);
+      })
+    );
   }
 }
